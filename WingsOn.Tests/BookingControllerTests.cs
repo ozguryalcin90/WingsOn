5b14a4f130ea0cfddea0f998ac25d507e41c35d9
Özgür Yalçın <ozgur.yalcin@yahoo.com>
2018-12-24 06:02:52 +0300

    BookingController and PersonController tests are added.

---
+++ WingsOn.Tests/BookingControllerTests.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System.Collections.Generic;
+using WingsOn.Dal.Abstract;
+using WingsOn.Domain;
+using WingsOn.WebApi.Controllers;
+using Xunit;
+
+namespace WingsOn.Tests
+{
+    public class BookingControllerTests
+    {
+        private Mock<IFlightRepository> mockFlightRepository;
+        private Mock<IBookingRepository> mockBookingRepository;
+
+        private BookingController bookingController;
+
+        public BookingControllerTests()
+        {
+            mockFlightRepository = new Mock<IFlightRepository>();
+            mockBookingRepository = new Mock<IBookingRepository>();
+
+            bookingController = new BookingController(mockFlightRepository.Object, mockBookingRepository.Object);
+        }
+
+        #region GetPassengers Method
+        [Fact]
+        public void GetPassengers_IfFlightNotFound_ReturnsNotFound()
+        {
+            mockFlightRepository.Setup(mock => mock.GetByFlightNumber("123")).Returns<StatusCodeResult>(null);
+
+            var result = bookingController.GetPassengers("123");
+            Assert.IsType<BadRequestObjectResult>(result.Result);
+        }
+
+        [Fact]
+        public void GetPassengers_WhenCalled_ReturnsOk()
+        {
+            Flight mockFlight = new Flight
+            {
+                Number = "123"
+            };
+
+            mockFlightRepository.Setup(mock => mock.GetByFlightNumber("123")).Returns(mockFlight);
+            
+            var result = bookingController.GetPassengers("123");
+            Assert.IsType<OkObjectResult>(result.Result);
+        }
+
+        [Fact]
+        public void GetPassengers_WhenCalled_ReturnsRightPassengers()
+        {
+            Flight mockFlight = new Flight
+            {
+                Number = "123"
+            };
+
+            mockFlightRepository.Setup(mock => mock.GetByFlightNumber("123")).Returns(mockFlight);
+
+            List<Booking> mockBookings = new List<Booking>
+            {
+                new Booking {
+                    Flight = mockFlight,
+                    Passengers = new List<Person>
+                    {
+                        new Person(),
+                        new Person()
+                    }
+                }
+            };
+
+            mockBookingRepository.Setup(mock => mock.GetBookings("123")).Returns(mockBookings);
+
+            var result = bookingController.GetPassengers("123").Result as OkObjectResult;
+            List<Person> passengers = Assert.IsType<List<Person>>(result.Value);
+
+            Assert.Equal(2, passengers.Count);
+        }
+        #endregion GetPassengers Method
+    }
+}
---
+++ WingsOn.Tests/PersonControllerTests.cs
@@ -0,0 +1,96 @@
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+using WingsOn.Dal.Abstract;
+using WingsOn.Domain;
+using WingsOn.WebApi.Controllers;
+using Xunit;
+
+namespace WingsOn.Tests
+{
+    public class PersonControllerTests
+    {
+        private Mock<IPersonRepository> mockPersonRepository;
+        private PersonController personController;
+        
+        public PersonControllerTests()
+        {
+             mockPersonRepository = new Mock<IPersonRepository>();
+             personController = new PersonController(mockPersonRepository.Object);
+        }
+
+        #region Get Method
+        [Fact]
+        public void Get_IfPersonNotFound_ReturnsNotFound()
+        {
+            mockPersonRepository.Setup(mock => mock.Get(1)).Returns<StatusCodeResult>(null);
+
+            var result = personController.Get(1).Result;
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Fact]
+        public void Get_WhenCalled_ReturnsOk()
+        {
+            mockPersonRepository.Setup(mock => mock.Get(1)).Returns(new Person());
+
+            var result = personController.Get(1);
+            Assert.IsType<OkObjectResult>(result.Result);
+        }
+
+        [Fact]
+        public void Get_WhenCalled_ReturnsRightPerson()
+        {
+            var person = new Person() { Id = 1 };
+            mockPersonRepository.Setup(mock => mock.Get(1)).Returns(person);
+
+            var result = personController.Get(1).Result as OkObjectResult;
+            var resultPerson = Assert.IsType<Person>(result.Value);
+
+            Assert.Equal(person, resultPerson);
+        }
+        #endregion Get Method
+
+        #region GetByGender Method
+        [Fact]
+        public void GetByGender_IfGenderNotValid_ReturnsBadRequest()
+        {
+            var result = personController.GetByGender("invalidGender");
+
+            Assert.IsType<BadRequestObjectResult>(result.Result);
+        }
+
+        [Fact]
+        public void GetByGender_WhenCalled_ReturnsOk()
+        {
+            var result = personController.GetByGender("female");
+            Assert.IsType<OkObjectResult>(result.Result);
+        }
+
+        [Fact]
+        public void GetByGender_WhenCalled_ReturnsRightAmountOfMales()
+        {
+            var mockPersons = new List<Person> { new Person { Gender = GenderType.Male }, new Person { Gender = GenderType.Male }, new Person { Gender = GenderType.Female } };
+            mockPersonRepository.Setup(mock => mock.GetByGender(GenderType.Male)).Returns(mockPersons);
+
+            var result = personController.GetByGender("male").Result as OkObjectResult;
+            var persons = Assert.IsType<List<Person>>(result.Value);
+
+            Assert.Equal(2, persons.Where(person => person.Gender == GenderType.Male).Count());
+        }
+
+        [Fact]
+        public void GetByGender_WhenCalled_ReturnsRightAmountOfFemales()
+        {
+            var mockPersons = new List<Person> { new Person { Gender = GenderType.Male }, new Person { Gender = GenderType.Male }, new Person { Gender = GenderType.Female } };
+            mockPersonRepository.Setup(mock => mock.GetByGender(GenderType.Female)).Returns(mockPersons);
+
+            var result = personController.GetByGender("female").Result as OkObjectResult;
+            var persons = Assert.IsType<List<Person>>(result.Value);
+
+            Assert.Equal(1, persons.Where(person => person.Gender == GenderType.Female).Count());
+        }
+        #endregion GetByGender Method
+    }
+}
